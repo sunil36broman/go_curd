9133187cd9cdd747907b5e9d668d4cb41ec77eba
sunil_broman <sunil.chandra@upaybd.com>
2024-10-03 21:22:37 +0600

    commit first

--- main.go
+++ main.go
@@ -1,7 +1,117 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+// User represents a user in the database
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+var db *sql.DB
+
+// Connect to the database
+func initDB() {
+	var err error
+	connStr := "host=localhost port=5432 user=root password=secret dbname=go_crud sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// CreateUser creates a new user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sqlStatement := `INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id`
+	id := 0
+	err = db.QueryRow(sqlStatement, user.Name, user.Email).Scan(&id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user.ID = id
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
+
+// GetUsers retrieves all users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT id, name, email FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+// UpdateUser updates a user's information
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sqlStatement := `UPDATE users SET name=$1, email=$2 WHERE id=$3`
+	_, err = db.Exec(sqlStatement, user.Name, user.Email, user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// DeleteUser deletes a user by ID
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	sqlStatement := `DELETE FROM users WHERE id=$1`
+	_, err := db.Exec(sqlStatement, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
 
 func main() {
-    fmt.Println("Hello, GitHub and Go!")
+	initDB()
+	defer db.Close()
+
+	http.HandleFunc("/users", CreateUser)        // Create user
+	http.HandleFunc("/users/get", GetUsers)      // Get users
+	http.HandleFunc("/users/update", UpdateUser) // Update user
+	http.HandleFunc("/users/delete", DeleteUser) // Delete user
+
+	fmt.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
